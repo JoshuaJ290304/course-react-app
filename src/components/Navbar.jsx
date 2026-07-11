c406266d338fa56bcc4ab7ff5c13c36b2454d795
Joshua J <jj4440024@gmail.com>
2026-07-11 14:30:31 +0530

    Highlight active navigation

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,15 +1,68 @@
 import React from 'react'
+import { Link,useLocation } from 'react-router-dom'
 
-const Navbar = () => {
-  return (
-    <nav className="navbar navbar-dark bg-dark">
-      <div className="container">
-        <span className="navbar-brand">
-          Course App
-        </span>
-      </div>
-    </nav>
-  )
+const Navbar=()=>{
+
+const location=useLocation()
+
+return(
+
+<nav className="navbar navbar-expand-lg navbar-dark bg-primary">
+
+<div className="container">
+
+<Link className="navbar-brand fw-bold" to="/">
+Course App
+</Link>
+
+<button
+className="navbar-toggler"
+type="button"
+data-bs-toggle="collapse"
+data-bs-target="#menu">
+
+<span className="navbar-toggler-icon"></span>
+
+</button>
+
+<div className="collapse navbar-collapse" id="menu">
+
+<ul className="navbar-nav ms-auto">
+
+<li className="nav-item">
+
+<Link
+className={`nav-link ${location.pathname==="/"?"active":""}`}
+to="/">
+
+Insert Course
+
+</Link>
+
+</li>
+
+<li className="nav-item">
+
+<Link
+className={`nav-link ${location.pathname==="/view"?"active":""}`}
+to="/view">
+
+View Courses
+
+</Link>
+
+</li>
+
+</ul>
+
+</div>
+
+</div>
+
+</nav>
+
+)
+
 }
 
 export default Navbar
