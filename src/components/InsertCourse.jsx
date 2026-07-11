975ae624ed9b6b115be4777c28a41b1d9e3cabf4
Joshua J <jj4440024@gmail.com>
2026-07-11 14:33:55 +0530

    Create Insert Course component

---
+++ src/components/InsertCourse.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const InsertCourse = () => {
+  return (
+    <div className="container mt-5">
+      <h2 className="text-center">Insert Course</h2>
+    </div>
+  )
+}
+
+export default InsertCourse
